3401b23b8fa4702fa6fd463acaa400b20a1a9643
beraer <s29315@pjwstk.edu.pl>
2025-03-27 03:06:46 +0100

    inital creation of class

---
+++ src/Vector.java
@@ -0,0 +1,9 @@
+public class Vector {
+    double[] input;
+    double[] target;
+
+    public Vector(double[] input, double[] target) {
+        this.input = input;
+        this.target = target;
+    }
+}
